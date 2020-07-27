62925d34ce4066f4d2f68443aa7c508dfb61b341
haha <QMBFXSL@gmail.com>
2020-07-28 11:55:56 +1200

    springa

---
+++ Assets/Scripts/HorseHolder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorseHolder : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] horsePrefabs;
+
+
+
+
+    public List<horseBehaviour> horseList = new List<horseBehaviour>();
+
+    public void AddHorse(horseBehaviour _hb)
+    {
+        bool horseExists = false;
+
+        foreach (horseBehaviour hb in horseList)
+        {
+            if(_hb.name == hb.name)
+            {
+                horseExists = true;
+            }
+        }
+
+        if (!horseExists)
+        {
+            horseBehaviour hnew = new horseBehaviour();
+            hnew.name = _hb.name;
+            horseList.Add(hnew);
+        }
+    }
+
+
+    public void InstantiateHorse(int horseIWant)
+    {
+
+    }
+
+
+}

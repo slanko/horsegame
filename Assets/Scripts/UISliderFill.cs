3bc98a116df1553b91b034d3ee89b8beb7499854
haha <QMBFXSL@gmail.com>
2020-07-30 08:55:53 +1200

    screeeee

---
+++ Assets/Scripts/UISliderFill.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UISliderFill : MonoBehaviour
+{
+    public Slider sliderBuddy;
+    Image img;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        img.fillAmount = sliderBuddy.value / 100;
+    }
+}

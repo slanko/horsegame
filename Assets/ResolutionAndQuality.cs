25f60a4ca773b632cd6f68270b33cbe166db98e2
Drina <Drinadomandiola@gmail.com>
2020-07-31 11:48:55 +1200

    WIP Settings menu

---
+++ Assets/ResolutionAndQuality.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResolutionAndQuality : MonoBehaviour
+{
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}

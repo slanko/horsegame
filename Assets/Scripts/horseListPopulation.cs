69505a0edcb6182cf0dbd6da161f537662ca20c7
haha <QMBFXSL@gmail.com>
2020-08-04 11:52:54 +1200

    seeya

--- Assets/Scripts/horseListPopulation.cs
+++ Assets/Scripts/horseListPopulation.cs
@@ -7,15 +7,30 @@ public class horseListPopulation : MonoBehaviour
 {
     public horseListScript theList;
     public GameObject horseListUI;
+    public Sprite bigImage;
 
     // Start is called before the first frame update
     void Start()
     {
+        populateList();
+    }
+
+
+    void populateList()
+    {
         int i = 0;
-        foreach(Transform child in transform)
+        foreach (Transform child in transform)
         {
-            child.name = i.ToString();
-            child.transform.Find("Image").GetComponent<Image>().sprite = theList.masterHorseList[i].smallPicture;
+            if(theList.masterHorseList[i].logged == true)
+            {
+                child.name = i.ToString();
+                child.transform.Find("Image").GetComponent<Image>().sprite = theList.masterHorseList[i].smallPicture;
+            }
+            else
+            {
+                child.name = "mystery horse....";
+                child.transform.Find("Image").GetComponent<Image>().sprite = bigImage;
+            }
             i++;
         }
     }

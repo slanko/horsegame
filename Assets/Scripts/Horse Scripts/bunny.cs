1362fb60feee9e3c22db27efc9b7e58fb67e0143
haha <QMBFXSL@gmail.com>
2020-08-18 11:51:31 +1200

    fefefefefefefefeef

--- Assets/Scripts/Horse Scripts/bunny.cs
+++ Assets/Scripts/Horse Scripts/bunny.cs
@@ -8,8 +8,12 @@ public class bunny : MonoBehaviour
     horseFunctionsScript hFS;
     godScript GOD;
     public float changeAmount;
+    horseBehaviour hB;
+    public float jumpForce = 10;
+    bool grounded;
     private void Start()
     {
+        hB = GetComponent<horseBehaviour>();
         hFS = GetComponent<horseFunctionsScript>();
         GOD = GameObject.Find("GOD").GetComponent<godScript>();
         rb = GetComponent<Rigidbody>();
@@ -18,9 +22,38 @@ public class bunny : MonoBehaviour
     {
         if (hFS.cC.currentState == characterController.playerState.TAMING)
         {
-            rb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             GOD.tamingSlider.value = GOD.tamingSlider.value + changeAmount;
             Invoke("meJump", Random.Range(hFS.tamingEventTimeBounds.x, hFS.tamingEventTimeBounds.y));
+        }
+    }
+
+
+    private void Update()
+    {
+        if (hB.beingRidden == true && hB.isTamed == true && grounded == true)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+                grounded = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "ground")
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag == "grounded")
+        {
+            grounded = false;
         }
     }
 }
